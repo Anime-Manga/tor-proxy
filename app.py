5b7989d49423ca82837515e18a6130a024c2d2f1
Simonepiano01 <simonedicarlo01@gmail.com>
2023-10-08 16:30:19 +0200

    Added: Native env var support on app.py for all arguments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 import signal
@@ -12,6 +13,17 @@ from utils import rabbitmq
 PRG_NAME = "animemanga-tor-privoxy"
 PRG_DESC = "Anime Manga - tor-privoxy container manager"
 PRG_EPIL = f"V1 - {PRG_NAME}"
+# ArgumentParser Defaults
+DEF_RABBIT_HOST = os.environ.get('ADDRESS_RABBIT', "localhost")
+DEF_RABBIT_PORT = os.environ.get('PORT_RABBIT', 5672)
+DEF_RABBIT_USER = os.environ.get('USERNAME_RABBIT', "guest")
+DEF_RABBIT_PASS = os.environ.get('PASSWORD_RABBIT', "guest")
+DEF_RABBIT_EXCH = os.environ.get('EXCHANGE_NAME', "")
+DEF_RABBIT_QUEUE = os.environ.get('QUEUE_RABBIT', "animemanga-tor-proxy")
+DEF_PROXY_REPLICAS = os.environ.get('REPLICAS', 15)
+DEF_EXPECTED_ADDR = os.environ.get('EXPECTED_ADDRESS', DEF_RABBIT_HOST)
+DEF_START_PORT = os.environ.get('START_PORT', 8000)
+DEF_PROXY_PATH = os.environ.get('PROXY_PATH', "proxy.txt")
 
 
 ### FUNCTIONS ###
@@ -52,17 +64,16 @@ def callback(ch, method, properties, body):
 ### ENTRY POINT ###
 def main():
     parser = ArgumentParser(PRG_NAME, description=PRG_DESC, epilog=PRG_EPIL)
-    parser.add_argument('--rabbit-host', required=True, help="ip/hostname of the RabbitMQ service")
-    parser.add_argument('--rabbit-port', default=5672, help="port of the RabbitMQ service [default: 5672]")
-    parser.add_argument('--rabbit-user', required=True, help="username of the RabbitMQ service")
-    parser.add_argument('--rabbit-pass', required=True, help="password of the RabbitMQ service")
-    parser.add_argument('--creds', default='creds', help="path to a file containing the credentials needed to connect to RabbitMQ")
-    parser.add_argument('--exchange-name', default='', help="name of the exchange that needs to be created")
-    parser.add_argument('--queue-name', default='animemanga-tor-proxy', help="name of the message queue that needs to be created")
-    parser.add_argument('--replicas', default=15, type=int, help="the amount of proxy containers that need to be created")
-    parser.add_argument('--expected-address', required=True, help="expected IP address from the message-queue (incoming messages address proxies using 'http://<expected-address>:<proxy_port>')")
-    parser.add_argument('--start-port', default=8000, type=int, help="the starting port the assign to the proxies (every proxy will have assigned a port number +1 of the previews)")
-    parser.add_argument('--proxy-file', default="proxy.txt", help="full path to the proxy file to be written")
+    parser.add_argument('--rabbit-host', default=DEF_RABBIT_HOST, help="ip/hostname of the RabbitMQ service")
+    parser.add_argument('--rabbit-port', default=DEF_RABBIT_PORT, help="port of the RabbitMQ service [default: 5672]")
+    parser.add_argument('--rabbit-user', default=DEF_RABBIT_USER, help="username of the RabbitMQ service")
+    parser.add_argument('--rabbit-pass', default=DEF_RABBIT_PASS, help="password of the RabbitMQ service")
+    parser.add_argument('--exchange-name', default=DEF_RABBIT_EXCH, help="name of the exchange that needs to be created")
+    parser.add_argument('--queue-name', default=DEF_RABBIT_QUEUE, help="name of the message queue that needs to be created")
+    parser.add_argument('--replicas', default=DEF_PROXY_REPLICAS, type=int, help="the amount of proxy containers that need to be created")
+    parser.add_argument('--expected-address', default=DEF_EXPECTED_ADDR, help="expected IP address from the message-queue (incoming messages address proxies using 'http://<expected-address>:<proxy_port>')")
+    parser.add_argument('--start-port', default=DEF_START_PORT, type=int, help="the starting port the assign to the proxies (every proxy will have assigned a port number +1 of the previews)")
+    parser.add_argument('--proxy-file', default=DEF_PROXY_PATH, help="full path to the proxy file to be written")
     args = parser.parse_args()
 
     try:
